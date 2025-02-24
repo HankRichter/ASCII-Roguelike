0f2d0bc1198b14159c303d36e15487c50fdcc7b9
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2025-02-24 14:41:31 -0500

    Adding the map.

--- init/roguelike.js
+++ init/roguelike.js
@@ -4,6 +4,7 @@ var ROWS = 10;
 var COLS = 15;
 
 var ACTORS = 10;
+var map;
 
 var game = new Phaser.Game(COLS * FONT * 0.6, ROWS * FONT, Phaser.AUTO, null, {
   create: create,
@@ -11,6 +12,7 @@ var game = new Phaser.Game(COLS * FONT * 0.6, ROWS * FONT, Phaser.AUTO, null, {
 
 function create() {
   game.input.keyboard.addCallbacks(null, null, onKeyUp);
+  initMap();
 }
 
 function onKeyUp(event) {
@@ -19,5 +21,17 @@ function onKeyUp(event) {
     case Keyboard.RIGHT:
     case Keyboard.UP:
     case Keyboard.DOWN:
+  }
+}
+
+function initMap() {
+  map = [];
+  for (var y = 0; y < ROWS; y++) {
+    var newRow = [];
+    for (var x = 0; x < COLS; x++) {
+      if (Math.random() > 0.8) newRow.push("#");
+      else newRow.push(".");
+    }
+    map.push(newRow);
   }
 }
