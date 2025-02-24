8818ecab867da9271d5f38a9db7605694ccc9f83
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2025-02-24 15:22:11 -0500

    Adding playable character and enemies.

--- init/roguelike.js
+++ init/roguelike.js
@@ -4,6 +4,11 @@ var COLS = 15;
 var ACTORS = 10;
 var map;
 var asciidisplay;
+var player;
+var actorList;
+var livingEnemies;
+
+var actorMap;
 
 var game = new Phaser.Game(COLS * FONT * 0.6, ROWS * FONT, Phaser.AUTO, null, {
   create: create,
@@ -21,6 +26,8 @@ function create() {
     }
   }
   drawMap();
+  initActors();
+  drawActors();
 }
 
 function onKeyUp(event) {
@@ -59,6 +66,39 @@ function drawMap() {
   for (var y = 0; y < ROWS; y++) {
     for (var x = 0; x < COLS; x++) {
       asciidisplay[y][x].content = map[y][x];
+    }
+  }
+}
+
+function randomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function initActors() {
+  actorList = [];
+  actorMap = [];
+
+  for (var e = 0; e < ACTORS; e++) {
+    var actor = { x: 0, y: 0, hp: e == 0 ? 3 : 1 };
+    do {
+      actor.y = randomInt(ROWS);
+      actor.x = randomInt(COLS);
+    } while (
+      map[actor.y][actor.x] == "#" ||
+      actorMap[actor.y + "_" + actor.x] != null
+    );
+    actorMap[actor.y + "_" + actor.x] = actor;
+    actorList.push(actor);
+  }
+  player = actorList[0];
+  livingEnemies = ACTORS - 1;
+}
+
+function drawActors() {
+  for (var a in actorList) {
+    if (actorList[a].hp > 0) {
+      asciidisplay[actorList[a].y][actorList[a].x].content =
+        a == 0 ? "" + player.hp : "e";
     }
   }
 }
